c2667f48f648aba189a6c5c8a75d72e2a6aa927e
Ari Susanto <risusanto@outlook.com>
2022-09-29 21:03:07 +0700

    prepare deploy to heroku

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const app = require('./app')
 
-const host = process.env.HOST || 'localhost';
+const host = process.env.HOST || '0.0.0.0';
 const port = process.env.PORT || '3000';
 
 app.listen(port, host, () => {
