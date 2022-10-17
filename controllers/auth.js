f549d850f8d359a3e034cc3533fbba28885f3fe9
Ari Susanto <risusanto@outlook.com>
2022-10-17 20:53:45 +0700

    add feature: check otp before registering user

--- controllers/auth.js
+++ controllers/auth.js
@@ -25,13 +25,20 @@ module.exports = {
             message: "",
             data: null
         }
-
         let user_data = {
             name: req.body.name,
             email: req.body.email,
             password: req.body.password,
             address: req.body.address
         }
+
+        let otp = await req.otpUC.verifyOTP(user_data.email,req.body.otp_code,"REGISTRATION")
+        if(otp === null) {
+            return res.status(400).json({
+                message: "invalid otp!"
+            })
+        }
+
         let register_data = await req.authUC.register(user_data)
         if(!register_data.is_success) {
             res_data.message = register_data.reason
@@ -41,6 +48,8 @@ module.exports = {
         res_data.status = 'success'
         res_data.message = 'ok'
         res_data.data = register_data.data
+
+        await req.otpUC.deleteAllOTP(user_data.email)
 
         res.json(res_data)
     }
